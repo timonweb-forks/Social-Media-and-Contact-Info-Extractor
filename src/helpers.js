565ac0d23ed2962be5fe2858152789b574ec0bf4
Guillim <guillim@users.noreply.github.com>
2021-10-04 11:41:16 +0200

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -15,7 +15,7 @@ async function extractUrlsFromPage(page, selector, sameDomain, urlDomain, pseudo
     const filteredLinks = allLinks.filter((url) => (sameDomain ? module.exports.getDomain(url) === urlDomain : true));
     //     we add the condition to match at least one pseudoUrl
     filteredLinks = filteredLinks.filter((url) => (
-        pseudoUrls.some( (pseudoUrl) => pseudoUrl.test(url) )
+        pseudoUrls.some( (pseudoUrl) => new RegExp(pseudoUrl).test(url) )
     ));
         
     log.info(`Found ${filteredLinks.length} links on ${page.url()}`);
